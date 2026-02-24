ea8d5bccebae787d4be19e23cf7be59f3f3193a8
Erangamadhushan <erangamadhushann956@gmail.com>
2026-02-24 10:11:57 +0530

    chore: update package.json to add zod as a peer dependency and include in devDependencies; update tsconfig.json to include jest types

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,11 +1,11 @@
-const { createDefaultPreset } = require("ts-jest");
-
-const tsJestTransformCfg = createDefaultPreset().transform;
-
-/** @type {import("jest").Config} **/
 module.exports = {
+  preset: "ts-jest/presets/default-esm",
   testEnvironment: "node",
+  extensionsToTreatAsEsm: [".ts"],
   transform: {
-    ...tsJestTransformCfg,
+    "^.+\\.ts$": ["ts-jest", { useESM: true }]
   },
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1"
+  }
 };
