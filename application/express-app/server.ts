c06e631f5aac471a2970b5ad3d197b028e889ab2
Erangamadhushan <erangamadhushann956@gmail.com>
2026-02-27 01:07:23 +0530

    feat: enhance demo application with MongoDB integration, Docker setup, and updated error handling

--- application/express-app/server.ts
+++ application/express-app/server.ts
@@ -6,9 +6,18 @@ import {
   asyncHandler,
   errorMiddleware,
 } from "@erangamadhushan/express-advanced-error-kit";
+import dotenv from "dotenv";
 
 const app = express();
 app.use(express.json());
+
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI as string)
+  .then(() => console.log("Mongo connected"))
+  .catch((err) => console.error("Mongo error:", err));
 
 // Simple route
 app.get("/error", (req, res) => {
@@ -60,6 +69,8 @@ app.use(
   }),
 );
 
-app.listen(5000, () => {
-  console.log("Demo server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Demo server running on port ${PORT}`);
 });
